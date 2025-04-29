e8c3a31a6c2bf4c1072691907309438b82d679c4
zehrashahp207 <shahpelengovazehra.com>
2025-04-29 15:57:23 +0000

    Fix: Completed the code for positive or negative check

--- python-hello_world/0-positive_or_negative.py
+++ python-hello_world/0-positive_or_negative.py
@@ -1,7 +1,6 @@
+#!/usr/bin/python3
 import random
-
-number = random.randint(-10, 10)  # Bu sətir dəyişməz qalır, təsadüfi ədəd təyin edir
-
+number = random.randint(-10, 10)
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
