eb44758c2238fe309f158b44a05957b5b5482059
zehrashahp207 <shahpelengovazehra.com>
2025-04-29 15:49:54 +0000

    Add executable script for 0-positive_or_negative.py

---
+++ python-hello_world/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+import random
+
+number = random.randint(-10, 10)  # Bu sətir dəyişməz qalır, təsadüfi ədəd təyin edir
+
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
