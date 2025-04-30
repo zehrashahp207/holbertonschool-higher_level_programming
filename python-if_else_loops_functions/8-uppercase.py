c6953d8d23c137f67164a40c7126c0cd93d6e0fa
zehrashahp207 <shahpelengovazehra>
2025-04-30 13:52:47 +0000

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            print(chr(ord(char) - 32), end="")
+        else:
+            print(char, end="")
+    print()
+
