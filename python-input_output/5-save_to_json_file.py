164e47b2e874254ec436941cc0d81e8045164b53
zehrashahp207 <shahpelengovazehra@gmail.com>
2025-06-03 13:44:01 +0000

    hgdjysgsuhgsh

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -2,6 +2,6 @@
 import json
 
 def save_to_json_file(my_obj, filename):
-    """Obyekti JSON formatında fayla yazır"""
+    """Write an object to a text file using JSON representation"""
     with open(filename, 'w') as f:
         json.dump(my_obj, f)
