e3404d616a6d6880fde194a8513c62bc1b30f831
zehrashahp207 <shahpelengovazehra@gmail.com>
2025-06-04 12:38:43 +0000

    8-classjson

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-class MyClass:
-    def __init__(self, name):
-        self.name = name
-        self.number = 0
+"""
+This module contains the function class_to_json that
+returns the dictionary description of a class instance
+for JSON serialization.
+"""
+
+def class_to_json(obj):
+    """Returns the dictionary description for JSON serialization of obj."""
+    return obj.__dict__
