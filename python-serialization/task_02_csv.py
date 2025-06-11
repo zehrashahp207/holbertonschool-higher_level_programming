60ebfb20d099347434484fb21f268d661f8a9187
zehrashahp207 <shahpelengovazehra@gmail.com>
2025-06-11 14:40:11 +0400

    taskk

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+def convert_csv_to_json(csv_filename):
+    try:
+        with open(csv_filename, mode='r', encoding='utf-8') as csv_file:
+            reader = csv.DictReader(csv_file)
+            data = list(reader)
+
+        with open('data.json', mode='w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        return True
+    except FileNotFoundError:
+        return False
+    except Exception:
+        return False
